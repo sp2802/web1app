c3f0c69a06a9fb0185abec10d11ab1e17ffb8c25
sp2802 <67074012+sp2802@users.noreply.github.com>
2021-01-05 01:46:08 +0530

    Add project files.

---
+++ web1app/Controllers/ContactController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web1app.Models;
+
+namespace web1app.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+new Contact(){Id=0,FirstName="Shailja",LastName="Pandey"},
+new Contact(){Id=1,FirstName="Prachi",LastName="Mittal"},
+new Contact(){Id=2,FirstName="Neha",LastName="Pande"}
+
+
+        };
+
+
+
+
+
+
+
+        // GET: api/Contact
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET: api/Contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+            if(contact == null)
+             { 
+                return NotFound(); 
+             }
+            return Ok(contact);
+        }
+
+        // POST: api/Contact
+        public IEnumerable<Contact> Post([FromBody]Contact newContact)
+        {
+            List<Contact> contactList = contacts.ToList < Contact>();
+            newContact.Id= contactList.Count;
+            contactList.Add(newContact);
+            contacts = contactList.ToArray();
+            return contacts;
+        }
+
+        // PUT: api/Contact/5
+        public IEnumerable<Contact> Put(int id, [FromBody]Contact updatedContact)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact != null)
+            {
+                contact.FirstName = updatedContact.FirstName;
+                contact.LastName = updatedContact.LastName;
+                //or else contact=updatedContact;
+            }
+            return contacts;
+        }
+
+        // DELETE: api/Contact/5
+        public IEnumerable<Contact> Delete(int id)
+        {
+            contacts = contacts.Where<Contact>(c => c.Id != id).ToArray<Contact>();
+            return contacts;
+        }
+    }
+}
---
+++ web1app/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web1app.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Contact()
+        {
+            Id = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
