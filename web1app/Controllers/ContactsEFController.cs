b882f52fc2b8737c66e3ee30938bdc2f44116ebb
sp2802 <67074012+sp2802@users.noreply.github.com>
2021-01-05 11:33:25 +0530

    EntityFrameworkController......

---
+++ web1app/Controllers/ContactsEFController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using web1app.Data;
+using web1app.Models;
+
+namespace web1app.Controllers
+{
+    public class ContactsEFController : ApiController
+    {
+        private web1appContext db = new web1appContext();
+
+        // GET: api/ContactsEF
+        public IQueryable<Contact> GetContacts()
+        {
+            return db.Contacts;
+        }
+
+        // GET: api/ContactsEF/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult GetContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // PUT: api/ContactsEF/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutContact(int id, Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ContactsEF
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult PostContact(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/ContactsEF/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult DeleteContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+
+            return Ok(contact);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContactExists(int id)
+        {
+            return db.Contacts.Count(e => e.Id == id) > 0;
+        }
+    }
+}
